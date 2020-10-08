423c3fb9fa86afe292a1e27e6bf7c23032c9e31d
Siarhei Drahichynski <siarhei.drahichynski@ontravelsolutions.com>
2020-10-08 17:03:01 +0300

    dream-team-one-reduce

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam (arr) {
+  if (!Array.isArray(arr)) return false;
+  
+  return arr.reduce((acc, curr, i, a) => {
+    if (!(typeof curr === 'string')) return acc;
+    acc.push(curr.trim().toUpperCase().charAt(0));
+    if (i === a.length - 1 ) return acc.sort().join('');
+    return acc;
+  }, []);
+}
+
